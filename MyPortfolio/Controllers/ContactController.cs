dfd4bccf867e532691e78241e0c00150c9a54cce
feridegunduz <gndz.frde@gmail.com>
2025-11-16 02:59:08 +0300

    değişiklikler1

---
+++ MyPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace MyPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public JsonResult Send(ContactFormModel model)
+        {
+            string message = "";
+            string color = "green";
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.From = new MailAddress(model.Email);
+                    mail.To.Add("gndz.frde@gmail.com"); 
+                    mail.Subject = string.IsNullOrEmpty(model.Subject) ? "No Subject" : model.Subject;
+                    mail.Body = $"Name: {model.Name}\nEmail: {model.Email}\n\nMessage:\n{model.Message}";
+
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                    smtp.Credentials = new NetworkCredential("gndz.frde@gmail.com", "dqlu yigl coiy tyuz"); // Gmail App Password
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+
+                    message = "Your message was sent, thank you!";
+                }
+                catch
+                {
+                    message = "Something went wrong. Please try again.";
+                    color = "red";
+                }
+            }
+            else
+            {
+                message = "Please fill in all required fields.";
+                color = "red";
+            }
+
+            // AJAX ile JSON dönecek
+            return Json(new { message = message, color = color });
+        }
+    }
+}
